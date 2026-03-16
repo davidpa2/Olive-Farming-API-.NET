6278dbbc2b16d9e9829f043e99413f4b3ea44d72
David Padilla <davidpaag2002@gmail.com>
2026-03-16 23:16:50 +0100

    feat(database): Adding EntityFramework configuration

---
+++ OliveFamingAPI/Data/FarmingDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using OliveFarmingAPI.Models;
+
+namespace OliveFarmingAPI.Data;
+
+public class FarmingDbContext: DbContext
+{
+    // Constructor
+    public FarmingDbContext(DbContextOptions<FarmingDbContext> options) : base(options)
+    {
+    }
+
+    // Tables at database
+    public DbSet<Season> Seasons { get; set; }
+    public DbSet<RainLog> RainLogs { get; set; }
+}
--- OliveFamingAPI/Program.cs
+++ OliveFamingAPI/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using OliveFarmingAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FarmingDbContext>(options =>
+    options.UseSqlite("Data Source=farming.db"));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
