c876e056a5c2e76bc41d9200e685b10fd550f261
David Padilla <davidpaag2002@gmail.com>
2026-03-22 23:00:53 +0100

    feat(app): Added swagger interface

--- OliveFamingAPI/Program.cs
+++ OliveFamingAPI/Program.cs
@@ -8,16 +8,16 @@ builder.Services.AddDbContext<FarmingDbContext>(options =>
 
 builder.Services.AddControllers();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -25,8 +25,3 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
