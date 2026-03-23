37309ed5a9c80a2603459fba89f167380f622b94
David Padilla <davidpaag2002@gmail.com>
2026-03-23 22:55:54 +0100

    feat(seasons): Added first structure of SeasonsController

---
+++ OliveFamingAPI/Controllers/SeasonsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OliveFarmingAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // Base route /api/Seasons
+public class SeasonsController : ControllerBase
+{
+
+}
