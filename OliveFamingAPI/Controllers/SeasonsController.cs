11bab36af173fda82f0cc941733c832e250a1998
David Padilla <davidpaag2002@gmail.com>
2026-03-25 23:13:22 +0100

    feat(seasons): Added GetAllSeasons endpoint

--- OliveFamingAPI/Controllers/SeasonsController.cs
+++ OliveFamingAPI/Controllers/SeasonsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OliveFarmingAPI.Data;
 
 namespace OliveFarmingAPI.Controllers;
 
@@ -8,4 +9,25 @@ namespace OliveFarmingAPI.Controllers;
 public class SeasonsController : ControllerBase
 {
 
+    private readonly FarmingDbContext _context;
+
+    // Inject BD connection
+    public SeasonsController(FarmingDbContext context)
+    {
+        _context = context;
+    }
+
+    // getAllSeasons -> GET: /api/Seasons
+    [HttpGet]
+    public async Task<IActionResult> GetAllSeasons()
+    {
+        var seasons = await _context.Seasons.Select(s => s.Name).ToListAsync();
+        
+        if (seasons == null)
+        {
+            return NotFound(new { errors = new[] { "No se han encontrado temporadas agrícolas" } });
+        }
+
+        return Ok(seasons);
+    }
 }
