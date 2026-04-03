1b541a30172d0e69add77bd430bd47ab9c10dbe3
David Padilla <davidpaag2002@gmail.com>
2026-04-03 13:30:23 +0200

    feat(users): Added user model and its migration to database

--- OliveFamingAPI/Migrations/20260403112742_AddUsers.Designer.cs
+++ OliveFamingAPI/Migrations/20260403112742_AddUsers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OliveFarmingAPI.Data;
 
@@ -10,9 +11,11 @@ using OliveFarmingAPI.Data;
 namespace OliveFamingAPI.Migrations
 {
     [DbContext(typeof(FarmingDbContext))]
-    partial class FarmingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260403112742_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
