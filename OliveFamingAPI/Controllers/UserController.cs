46c850f01fa8ffb18968405b7a3583a93a0fae19
David Padilla <davidpaag2002@gmail.com>
2026-04-04 12:25:52 +0200

    feat(user): Added register endpoing

--- OliveFamingAPI/Controllers/UserController.cs
+++ OliveFamingAPI/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OliveFarmingAPI.Data;
+using OliveFarmingAPI.Models;
+using OliveFarmingAPI.DTOs;
 
 namespace OliveFarmingAPI.Controllers;
 
@@ -8,9 +11,35 @@ namespace OliveFarmingAPI.Controllers;
 public class UsersController : ControllerBase
 {
     private readonly FarmingDbContext _context;
+    private readonly IConfiguration _config;
 
-    public UsersController(FarmingDbContext context)
+    public UsersController(FarmingDbContext context, IConfiguration config)
     {
         _context = context;
+        _config = config; // Inyect configuration to read appsettings.json
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] UserRegisterDTO userDto)
+    {
+        var userExists = await _context.Users.AnyAsync(u => u.Email == userDto.Email);
+        if (userExists)
+            return BadRequest(new { errors = new[] { "Ya existe un usuario con ese email registrado" } });
+
+        // Hash password
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+
+        var newUser = new User
+        {
+            Email = userDto.Email,
+            Name = userDto.Name,
+            Surname = userDto.Surname,
+            PasswordHash = passwordHash
+        };
+
+        _context.Users.Add(newUser);
+        await _context.SaveChangesAsync();
+
+        return Ok("Se ha registrado el usuario");
     }
 }
